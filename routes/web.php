333a69a91331f541d74b85f8188251442f754d1a
nhutnguyen1198 <nm11nhut@gmail.com>
2026-01-12 13:45:42 +0700

    commit fix

--- routes/web.php
+++ routes/web.php
@@ -8,53 +8,41 @@ Route::get('/', function () {
     return view('ui-index.index');
 })->middleware('track.visitor');
 
-Route::prefix('admin')
-    ->name('admin.')
-    ->group(function () {
+Route::prefix('admin')->group(function () {
 
-        Route::middleware('admin.guest')->group(function () {
-            Route::get('login', fn() => view('admin.auth.login'))
-                ->name('login');
-        });
-
-        Route::middleware('admin.auth')->group(function () {
-
-            Route::get('dashboard', fn() => view('admin.dashboard'))
-                ->name('dashboard');
-
-            Route::get('users', fn() => view('crud-user.list'))
-                ->name('users.list');
-
-            // POSTS
-            Route::resource('posts', PostController::class);
-
-            // CATEGORIES
-            Route::get('categories', [CategoryController::class, 'index'])
-                ->name('categories.index');
-
-            Route::get('categories/create', [CategoryController::class, 'create'])
-                ->name('categories.create');
-
-            Route::post('categories', [CategoryController::class, 'store'])
-                ->name('categories.store');
-
-            Route::get('categories/{id}/edit', [CategoryController::class, 'edit'])
-                ->name('categories.edit');
-
-            Route::put('categories/{id}', [CategoryController::class, 'update'])
-                ->name('categories.update');
-
-            Route::delete('categories/{id}', [CategoryController::class, 'destroy'])
-                ->name('categories.destroy');
-        });
+    Route::middleware('admin.guest')->group(function () {
+        Route::get('login', fn() => view('admin.auth.login'))->name('admin.login');
     });
 
-// FRONTEND
-Route::get('/single', [App\Http\Controllers\IndexController::class, 'single'])->name('single');
+    Route::middleware('admin.auth')->group(function () {
+        // Route::get('dashboard', fn() => view('layouts.dashboard'))->name('dashboard');
+
+        Route::get('dashboard', fn() => view('admin.dashboard'))->name('admin.dashboard');
+
+        Route::get('users', fn() => view('crud-user.list'))->name('user.list');
+
+        Route::resource('posts', PostController::class);
+
+        //category
+        Route::get('categories', [CategoryController::class, 'index'])->name('category.list');
+
+        Route::get('categories/create', [CategoryController::class, 'create'])->name('category.create');
+
+        Route::post('categories', [CategoryController::class, 'store'])->name('category.store');
+
+        Route::get('categories/{id}/edit', [CategoryController::class, 'edit'])->name('category.edit');
+
+        Route::put('categories/{id}', [CategoryController::class, 'update'])->name('category.update');
+
+        Route::delete('categories/{id}', [CategoryController::class, 'destroy'])->name('category.destroy');
+    });
+});
+Route::get('/single', [App\Http\Controllers\IndexController::class, 'single'])->name('single'); 
 Route::get('/contact', [App\Http\Controllers\IndexController::class, 'contact'])->name('home.contact');
 Route::get('/home', [App\Http\Controllers\IndexController::class, 'index'])->name('home');
 Route::get('/news', [App\Http\Controllers\IndexController::class, 'news'])->name('home.news');
 Route::get('/events', [App\Http\Controllers\IndexController::class, 'events'])->name('home.events');
 Route::get('/plan', [App\Http\Controllers\IndexController::class, 'plan'])->name('home.plan');
 Route::get('/explore', [App\Http\Controllers\IndexController::class, 'explore'])->name('home.explore');
-Route::get('/post_details', [App\Http\Controllers\IndexController::class, 'post_details'])->name('home.post_details');
+Route::get('/single', [App\Http\Controllers\IndexController::class, 'single'])->name('single')->middleware('track.visitor');
+
