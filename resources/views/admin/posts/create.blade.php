a1384453b3d7995db69cdc319b86edab1eb7a07b
huyvo79 <vohaanhhuy79@gmail.com>
2026-02-03 13:52:10 +0700

    add video

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -142,7 +142,7 @@
 <script src="{{ asset('js/tinymce/tinymce.min.js') }}"></script>
 <script>
 tinymce.init({
-    selector: '#content_editor',
+    selector: '#content-editor',
     license_key: 'gpl',
     height: 500,
     plugins: 'image link lists table code preview fullscreen wordcount media', 
