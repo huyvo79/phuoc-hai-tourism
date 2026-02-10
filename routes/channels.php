14dc148bcc1c12bc520f069b89a05af0a51dd33b
huyvo79 <vohaanhhuy79@gmail.com>
2026-02-11 00:04:20 +0700

    a

--- routes/channels.php
+++ routes/channels.php
@@ -2,11 +2,6 @@
 
 use Illuminate\Support\Facades\Broadcast;
 
-// Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-//     return (int) $user->id === (int) $id;
-// });
-
-// // Cho phép bất kỳ ai có session ID đúng đều được nghe kênh này
-// Broadcast::channel('chat.{sessionId}', function ($user, $sessionId) {
-//     return true; // Luôn trả về true để Guest cũng nghe được
-// });
+Broadcast::channel('chat.{receiverId}', function ($user, $receiverId) {
+    return (int) $user->id === (int) $receiverId;
+});
